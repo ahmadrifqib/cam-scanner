2467fd9c30ac37509686a2ca4625040cda98ded0
Ahmad Rifqi Baidhowi <ahmad.rifqi@outlook.co.id>
2021-09-16 20:01:32 +0700

    first build

---
+++ src/components/QRCodeScreen.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="d-flex h-screen bg-black">
+      <div class="w-4/5 h-full m-auto">
+        <qrcode-stream @decode="onDecode"/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { QrcodeStream } from 'vue3-qrcode-reader'
+import axios from "axios"
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+export default {
+  components: {
+      QrcodeStream
+  },
+  methods: {
+    async onDecode(decodeString){
+      console.log(decodeString)
+      // try {
+      //   const a = await axios.post("https://example.com", decodeString);
+      //   console.log(a);
+      // } catch (error) {
+      //   throw Error(error);
+      // }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import Vue from "vue";
+// import VueQrcodeReader from "vue-qrcode-reader";
+import "tailwindcss/tailwind.css";
+// const axios = require("axios").default;
 
-createApp(App).mount('#app')
+// Vue.use(VueQrcodeReader);
+
+// import axios from "axios";
+//axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+// axios.defaults.baseURL = "https://example.com";
+
+createApp(App).mount("#app");
